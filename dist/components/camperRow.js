3ff7ca802b9cf55a1686e4c3ba782800f198a298
Greg Duncan <gregdd@outlook.com>
2017-05-29 11:41:49 +1000

    remove comments

--- dist/components/camperRow.js
+++ dist/components/camperRow.js
@@ -13,12 +13,3 @@ const CamperRow = (props) => {
 }
 
 export default CamperRow
-
-/*
-You can't modify props in a component
-So what you're doing here is adding some new state to your component. But the stateProp that you pass into the component as props is immutable
-If you want to update the stateProp, you need to pass in a function that edits the state in the parent component, and then passes back in the update prop to the child component
-Daniel Tait
-Daniel
-Also if you want to store the name of an object property in a variable you have to use bracket notation to access that property and the only way you could do it that way would be by changing state without using setState (don't do that) or by making a copy of state and altering that one property like stateCopy[stateProp] = data; this.setState(stateCopy);
-*/

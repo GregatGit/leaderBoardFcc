75d9adf205aec14ab4da57614849b0750c4e3cc1
Greg Duncan <gregdd@outlook.com>
2017-06-13 11:47:07 +1000

    added basic css and scss via webpack

--- dist/components/camperRow.js
+++ dist/components/camperRow.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import '../css/camperRowStyle.css'
+import '../css/rowImg.scss'
 
 const CamperRow = (props) => {
   let url = `https://freecodecamp.com/${props.name}`
   return (
-    <tr>
+    <tr className='rowLine'>
       <th>{props.position}</th>
       <th>
         <a href={url}target="_blank">
-        <img src={props.img} width="50" height="50"/></a>
+        <img src={props.img} className='rowImg' width="50" height="50"/></a>
         {props.name}
       </th>
       <th>{props.recentPoints}</th>
